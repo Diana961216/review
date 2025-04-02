d6027a4739aec83af25b772e9b1df1ac92c05059
DianaArman1216 <dianmap96@icloud.com>
2025-04-01 22:09:42 -0400

    complete first break

---
+++ scratch.js
@@ -0,0 +1,32 @@
+// thinking in JavaScript
+// this is a comment, a single line comment
+/* this is a multi-line comment
+dsjfbjsdf
+fsdfsd
+fsdf
+sdfds
+fsdf
+* */
+
+// How to print to the console, the second most important thing to learn in any language.
+console.log("I'm a real boy")
+
+// example of hoisting
+console.log (thing = "something")
+// variables
+// Declared, using very specific words. 
+// let- a mutable variable, the value can be changed. This may be reassigned. 
+let myName = "Kyle" // String begins with ", ', `, and contain characters
+// = is the assignment operator, this DOES NOT represent equality
+
+// const- is a immutable variable, meaning it cannot be reassigned. 
+const theSky = "is blue" 
+
+// var- is mutable, and is contained in the global scope.
+// It receives the super power of hoisting. 
+var thing = "hoisted"
+
+
+
+
+ 
